5322544548b3f41b33786f7413479d49df522620
rodgef <rodgef101@gmail.com>
2019-03-08 13:02:01 +0200

    possible optimisations needed

--- d11/d11.py
+++ d11/d11.py
@@ -5,13 +5,4 @@ def getAllSuffixes(mylist, prefix):
 	for i in lst:
 		if i.startswith(prefix):
 			ret.append(i)
-
 	return ret
-
-if __name__ == '__main__':
-	val = ["abc","dab", "a"]
-	for  i in range(1000000):
-		val.append(chr(i % 128) + "ab")
-	print(getAllSuffixes(val,"ab"))
-		
-	
