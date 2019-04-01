8a1da62875949459ba8b9d188f48aa8a24f508c3
rodgef <rodgef101@gmail.com>
2019-04-01 10:36:21 +0200

    added incomplete version

--- d13/d13.py
+++ d13/d13.py
@@ -6,4 +6,24 @@ def get_all_substrings(s):
 			ret.append(s[j:j + i])
 	return ret
 
-print(get_all_substrings("abc"))
+def char_count(st):
+	return len(set(st))
+
+def len_most_unique_chars(s,n):
+	ret_lst = []
+	lst = get_all_substrings(s)
+	for  i in lst:
+		if char_count(i) == n:
+			ret_lst.append(i)
+	return ret_lst
+
+#print(
+subs = get_all_substrings("abc")
+f = len_most_unique_chars(subs,2)
+a = []
+for x in f:
+	#if char_count(x) == 2:
+	#	a.append(x)
+	print(x)
+print(a)
+
