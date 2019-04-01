6dfca1eda9ab5864a7c75b36a25cdecaa7902bf5
rodgef <rodgef101@gmail.com>
2019-04-01 09:59:34 +0200

    fixed incremental issue in get_all_substrings

--- d13/d13.py
+++ d13/d13.py
@@ -2,8 +2,8 @@ def get_all_substrings(s):
 	ret = []
 	n = len(s)
 	for i in range(n):
-		for j in range(n - i):
-			ret.append(s[j:j + i-1])
+		for j in range(n - i + 1):
+			ret.append(s[j:j + i])
 	return ret
 
 print(get_all_substrings("abc"))
